8923ac4b5797deb71fb785471f4c0acb0a71430e
Denis Beurivé <denis.beurive@gmail.com>
2023-02-14 21:17:52 +0100

    review the documentation

--- src/command_line/mod.rs
+++ src/command_line/mod.rs
@@ -6,44 +6,59 @@ pub mod parameter;
 
 type Name = String;
 
+/// A command line specification.
 pub struct CommandLine<'a> {
-    /// Optional parameters with an "explicit" value.
-    /// Ex: "--input=<value>" or "-i=<value>".
+    /// An "action" defines a "sub command line".
+    /// Ex: `git add ...`, `git commit ...`
+    ///     "add" and "commit" are "actions".
+    /// Please note that an "action" must follow the executable name or another "action".
+    actions: HashMap<Name, &'a CommandLine<'a>>,
+    /// Optional parameters with an "explicit" value - that is "options".
+    /// Ex: `--input=<value>` or `-i=<value>`.
     option: HashMap<Name, OptionalParameter>,
-    /// Optional parameters with an "implicit" value (true/set or false/unset).
-    /// Ex: "--verbose" or "-v".
+    /// Optional parameters with an "implicit" value (true/false) - that is "flags".
+    /// Ex: `--verbose` or `-v`.
     flag: HashMap<Name, OptionalParameter>,
     /// Mandatory parameters that appear at the end of the command line.
     /// Please note that the declaration order for these parameters is important.
-    parameter: IndexMap<Name, MandatoryParameter>,
-    actions: HashMap<Name, &'a CommandLine<'a>>
+    parameter: IndexMap<Name, MandatoryParameter>
 }
 
 impl<'a> CommandLine<'_> {
     pub fn new() -> CommandLine<'a> {
         return CommandLine {
+            actions: HashMap::new(),
             option: HashMap::new(),
             flag: HashMap::new(),
-            parameter: IndexMap::new(),
-            actions: HashMap::new()
+            parameter: IndexMap::new()
         }
     }
 
-    pub fn add_option(&mut self, name: String, long: Option<String>, short: Option<char>, max_occurrence:Option<u8>) {
+    pub fn add_option(&mut self,
+                      name: String,
+                      long: Option<String>,
+                      short: Option<char>,
+                      max_occurrence:Option<u8>) {
         if self.option.contains_key(name.as_str()) {
             panic!("An option with the name {} already exists!", name)
         }
         self.option.insert(name, OptionalParameter::new(long, short, max_occurrence));
     }
 
-    pub fn add_flag(&mut self, name: String, long: Option<String>, short: Option<char>, max_occurrence:Option<u8>) {
+    pub fn add_flag(&mut self,
+                    name: String,
+                    long: Option<String>,
+                    short: Option<char>,
+                    max_occurrence:Option<u8>) {
         if self.option.contains_key(name.as_str()) {
             panic!("A flag with the name {} already exists!", name)
         }
         self.flag.insert(name, OptionalParameter::new(long, short, max_occurrence));
     }
 
-    pub fn add_parameter(&mut self, name: String, max_occurrence: Option<u8>) {
+    pub fn add_parameter(&mut self,
+                         name: String,
+                         max_occurrence: Option<u8>) {
         if self.parameter.contains_key(name.as_str()) {
             panic!("A parameter with the name {} already exists!", name)
         }
@@ -61,7 +76,9 @@ impl<'a> CommandLine<'_> {
         self.parameter.insert(name, MandatoryParameter::new(max_occurrence));
     }
 
-    fn add_action(&mut self, name: String, command_line: Option<CommandLine>) -> CommandLine {
+    fn add_action(&mut self,
+                  name: String,
+                  command_line: Option<CommandLine>) -> CommandLine {
         if self.actions.contains_key(name.as_str()) {
             panic!("An action with the name {} already exists!", name)
         }
--- src/command_line/parameter.rs
+++ src/command_line/parameter.rs
@@ -1,17 +1,22 @@
 
-/// An optional parameter with an "explicit" value is called an "option". Ex: "--input=<value>" or
-/// "-i=<value>"
+/// Optional parameter
 ///
-/// An optional parameter without "explicit" value has an "implicit" value. The value is a boolean
-/// that indicates whether the parameter appears on the command line or not). Optional parameters
-/// with "implicit" values are called "flags". Ex: "--verbose" or "-v".
+/// An optional parameter with an "explicit" value is called an "option". Ex: `--input=<value>` or
+/// `-i=<value>`
+///
+/// An optional parameter without (explicit) value is called a "flag". Ex: `--verbose` or `-v`.
+/// A "flag" has an "implicit" value. The value is a boolean that indicates whether the parameter
+/// appears on the command line or not).  Ex: `--verbose` or `-v`.
+///
+/// An optional parameter may be associated with a "long identifier" (`--input=<value>` or
+/// `--verbose`), a short identifier (`-i=<input>` or `-v`), or both.
 pub struct OptionalParameter {
     /// A string that represents the "long identifier" for this optional parameter.
-    /// For example, is the long option appears as "--input" on the command line, then the value
+    /// For example, if the long option appears as `--input` on the command line, then the value
     /// of this field is "input".
     long: Option<String>,
     /// A string that represents the "short identifier" for this optional parameter.
-    /// For example, if the short option appears as "-i" on the command line, then the value
+    /// For example, if the short option appears as `-i` on the command line, then the value
     /// of this field is "i".
     short: Option<char>,
     /// The maximum number of times that this optional parameter can appear within the command line.
@@ -19,17 +24,16 @@ pub struct OptionalParameter {
     max_occurrence: u8
 }
 
+/// A mandatory parameter appears at the end of the command line, after all optional parameters
+/// Mandatory parameters are identified by their positions on the command line.
 pub struct MandatoryParameter {
     /// The maximum number of times that this parameter can appear within the command line.
     /// If the parameter is the last of the command line, then it can appear more than once.
     /// Otherwise, it appears once.
-    max_occurrence: u8
-}
-
-fn get_max_occurrence(max_occurrence: Option<u8>) -> u8 {
-    if max_occurrence.is_none() { return 1; }
-    if 0 == max_occurrence.unwrap() { return 1 }
-    return max_occurrence.unwrap();
+    ///
+    /// If the value of this field is None, it means that this mandatory parameter can appear up
+    /// to 255 times on the command line (the maximum value for an `u8`).
+    max_occurrence: Option<u8>
 }
 
 /// Check the (long and short) identifiers of an optional parameter.
@@ -42,25 +46,31 @@ fn get_max_occurrence(max_occurrence: Option<u8>) -> u8 {
 // }
 
 impl OptionalParameter {
-    pub fn new(long: Option<String>, short: Option<char>, max_occurrence:Option<u8>) -> OptionalParameter {
+    fn get_max_occurrence(in_max_occurrence: Option<u8>) -> u8 {
+        if in_max_occurrence.is_none() { return 1; }
+        let max_occurrence = in_max_occurrence.unwrap();
+        if 0 == max_occurrence { return 1 }
+        return max_occurrence;
+    }
 
-        if long.is_none() && short.is_none() {
+    pub fn new(in_long: Option<String>, in_short: Option<char>, in_max_occurrence:Option<u8>) -> OptionalParameter {
+        if in_long.is_none() && in_short.is_none() {
             panic!("Long and short optional parameters identifiers cannot be undefined simultaneously")
         }
-        if long.is_some() {
-            if 0 == long.as_ref().unwrap().len() { panic!("A long parameter identifier cannot not be an empty string"); }
+        if in_long.is_some() {
+            if 0 == in_long.as_ref().unwrap().len() { panic!("A long parameter identifier cannot not be an empty string"); }
         }
-
-        return OptionalParameter { long, short, max_occurrence: get_max_occurrence(max_occurrence) };
+        return OptionalParameter { long: in_long, short: in_short, max_occurrence: get_max_occurrence(in_max_occurrence) };
     }
 }
 
 impl MandatoryParameter {
-    pub fn new(max_occurrence: Option<u8>) -> MandatoryParameter {
-        return MandatoryParameter { max_occurrence: get_max_occurrence(max_occurrence) };
+    pub fn new(in_max_occurrence: Option<u8>) -> MandatoryParameter {
+        if 0 == in_max_occurrence { panic!("A mandatory parameter must appear at least one time on the command line") }
+        return MandatoryParameter { max_occurrence: in_max_occurrence };
     }
 
-    pub fn get_max_occurrence(&self) -> u8 {
+    pub fn get_max_occurrence(&self) -> Option<u8> {
         return self.max_occurrence;
     }
 }
